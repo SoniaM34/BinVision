d5e42a8674b5eb9be645038d47700206ed7e4813
67son <67soniamarquez@gmail.com>
2023-10-25 09:29:12 +0200

    récupération des fichiers

---
+++ android/app/src/main/kotlin/com/example/binvision/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.binvision
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const AccueilPage(),
+    );
+  }
+}
+
+class AccueilPage extends StatelessWidget {
+  const AccueilPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Accueil"), // Titre de la page
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset('assets/BV_icon.png'),
+            Text(
+              "Active ta localisation",
+              style: TextStyle(fontSize: 24.0),
+            ),
+            Text(
+              "OU",
+              style: TextStyle(fontSize: 24.0),
+            ),
+            Text(
+              "Indique ton code postal",
+              style: TextStyle(fontSize: 24.0),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Naviguer vers la page suivante
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => PageSuivante()),
+                );
+              },
+              child: Text("SUIVANT"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PageSuivante extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Page Suivante"),
+      ),
+      body: Center(
+        child: Text(
+          "Contenu de la page suivante",
+          style: TextStyle(fontSize: 24.0),
+        ),
+      ),
+    );
+  }
+}
